cafeeb0339cc1418b28da356ecdeb181c48e075a
Ardian Fazri <ardiannurfazri@gmail.com>
2024-10-20 11:00:33 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SocialLinksProfileComponent } from './components/social-links-profile/social-links-profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    SocialLinksProfileComponent,
+    RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
